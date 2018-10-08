e644923e9d9a7ff529fab0f2ab79a9f177b3208a
Perrine LÉQUIPÉ <perrine.lequipe@smile.fr>
2018-10-08 14:14:57 +0200

    [Front] Initialisation du thème ('peleq') + mise en place de l'arche du site * Initialisation (S)CSS, JS, template _master + _index * Initialisation du menu gauche et des menus de footer * Design et templating : header / sidebar / footer

---
+++ public/theme/peleq/js/asmb.js
@@ -0,0 +1,73 @@
+let isTouchScreenDevice = false;
+let isSmallScreen = false;
+
+$(window).on('load', function () {
+    if ("ontouchstart" in document.documentElement) {
+        isTouchScreenDevice = true;
+    }
+    if ($(window).width() <= 800) {
+        isSmallScreen = true;
+    }
+
+    if (isSmallScreen) {
+        let $sidebar = $('#sidebar'),
+            openSideBar = function () {
+                $sidebar.addClass('active');
+                $('body').addClass('overflow-hidden');
+            },
+            closeSidebar = function () {
+                $sidebar.removeClass('active');
+                $('body').removeClass('overflow-hidden');
+            };
+
+        $('#sidebar').css('visibility', 'visible')
+        $('#openSidebar').on('click', openSideBar);
+        $('#closeSidebar').on('click', closeSidebar);
+
+        // Behaviors for TOUCH device only
+        if (isTouchScreenDevice) {
+            $sidebar.swipe({
+                swipeLeft: function (event, direction, distance, duration, fingerCount) {
+                    // if (phase == "move" && direction == "right") {
+                    //     openSideBar();
+                    //     return false;
+                    // }
+                    if (direction == "left" && distance > 75) {
+                        closeSidebar();
+                        return false;
+                    }
+                }
+            });
+        }
+    }
+
+    // Behaviors for NON-TOUCH device only
+    if (!isTouchScreenDevice) {
+        // Touch device have nice scroll bar, we don't handle it.
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal",
+            contentTouchScroll: 0,
+            documentTouchScroll: 0
+        });
+    }
+
+
+    // Add "sticky" mode only if html content height is X more high than window height, X = 250px;
+    $(window).scroll(function () {
+        if ($(document).height() - $(window).height() > 200) {
+            let $header = $('#header'),
+                $main = $('#main'),
+                scrollTop = $(this).scrollTop();
+
+            if (scrollTop > 20) {
+                $header.addClass('sticky');
+                $main.addClass('under-sticky');
+            } else {
+                $header.removeClass('sticky');
+                $main.removeClass('under-sticky');
+            }
+        }
+    });
+});
+
+
---
+++ public/theme/peleq/lib/bootstrap-4.1.3/peleq.md
@@ -0,0 +1,1 @@
+* Le fichier bootstrap.scss a été modidié pour corriger un bug.
