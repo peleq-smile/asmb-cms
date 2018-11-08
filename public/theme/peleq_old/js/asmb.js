c415bae377818d4ccba0d9752a0c72e68c1e1964
Perrine LÉQUIPÉ <perrine.lequipe@smile.fr>
2018-11-08 13:14:27 +0100

    [Theme] Réinitialisation du thème, basé sur Bolt : thème avec menu de navigation haut et non gauche.

--- public/theme/peleq_old/js/asmb.js
+++ public/theme/peleq_old/js/asmb.js
@@ -1,7 +1,12 @@
 let isTouchScreenDevice = false;
 let isSmallScreen = false;
+// Delta in px between Viewport Height and Full Height
+let deltaWpAndFh = 250;
 
 $(window).on('load', function () {
+    let $sidebar = $('#sidebar'),
+        sidebarWidth = $sidebar.width();
+
     if ("ontouchstart" in document.documentElement) {
         isTouchScreenDevice = true;
     }
@@ -10,8 +15,7 @@ $(window).on('load', function () {
     }
 
     if (isSmallScreen) {
-        let $sidebar = $('#sidebar'),
-            openSideBar = function () {
+        let openSideBar = function () {
                 $sidebar.addClass('active');
                 $('body').addClass('overflow-hidden');
             },
@@ -20,7 +24,7 @@ $(window).on('load', function () {
                 $('body').removeClass('overflow-hidden');
             };
 
-        $sidebar.css('visibility', 'visible')
+        $sidebar.css('visibility', 'visible');
         $('#openSidebar').on('click', openSideBar);
         $('#closeSidebar').on('click', closeSidebar);
 
@@ -40,30 +44,60 @@ $(window).on('load', function () {
     // Behaviors for NON-TOUCH device only
     if (!isTouchScreenDevice) {
         // Touch device have nice scroll bar, we don't handle it.
-        $("#sidebar").mCustomScrollbar({
+        $("#sidebarMenu").mCustomScrollbar({
             theme: "minimal",
             contentTouchScroll: 0,
             documentTouchScroll: 0
         });
+
+        // Detect width of sidebar and set it as CSS property (and for search form too)
+        // $sidebar.css('width', sidebarWidth);
+        // $sidebar.find('#searchform-inline').css('width', sidebarWidth);
     }
 
-
     // Add "sticky" mode only if html content height is X more high than window height, X = 250px;
-    $(window).scroll(function () {
-        if ($(document).height() - $(window).height() > 200) {
-            let $header = $('#header'),
-                $main = $('#main'),
-                scrollTop = $(this).scrollTop();
+    // @see https://gist.github.com/toshimaru/6102647
+    $.fn.isInViewport = function () {
+        let elementTop = $(this).offset().top;
+        let elementBottom = elementTop + $(this).outerHeight();
 
-            if (scrollTop > 20) {
+        let viewportTop = $(window).scrollTop();
+        let viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    let docHeight = $(document).height(),
+        $header = $('#header'),
+        $main = $('#main'),
+        $footer = $('#footer');
+
+    let handleScroll = function () {
+        if (docHeight - $(window).height() > deltaWpAndFh) {
+            let scrollTop = $(window).scrollTop();
+
+            // Handle when start to scrolling from top
+            if (scrollTop > 40) {
                 $header.addClass('sticky');
                 $main.addClass('under-sticky');
             } else {
                 $header.removeClass('sticky');
                 $main.removeClass('under-sticky');
             }
+
+            // Handle when we scrolling close to the footer
+            if ($footer.isInViewport()) {
+                // $sidebar.addClass('absolute-sidebar');
+            } else {
+                // $sidebar.removeClass('absolute-sidebar');
+            }
         }
-    });
+    };
+
+    // After windows loading : handle scroll
+    handleScroll();
+
+    $(window).scroll(handleScroll);
 });
 
 
