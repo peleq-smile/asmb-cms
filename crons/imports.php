6098ec163c0fae4fe6b4cc7e38f45b583b85ad82
Perrine Léquipé <perrine.lequipe@smile.fr>
2019-09-09 13:37:15 +0200

    [Compétitions] Ajout d'un script php de crons pour l'import des données GS

---
+++ crons/imports.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+/** @var \Silex\Application $app */
+use Symfony\Component\Console\Input\StringInput;
+
+$app = require __DIR__ . '/../vendor/bolt/bolt/app/bootstrap.php';
+$app->boot();
+
+/** @var \Symfony\Component\Console\Application $nut Nut Console Application */
+$nut = $app['nut'];
+
+try {
+    $nut->run(new StringInput('asmb:competition:refresh'));
+} catch (Exception $e) {
+    echo $e->getMessage() . "\n";
+}
