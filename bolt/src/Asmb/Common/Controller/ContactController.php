9f97ec7e8d4bad6023030dfdfbc7d7d5f05fc2d4
Perrine Léquipé <perrine.lequipe@rosemood.fr>
2022-05-30 14:31:11 +0200

    [CONTACT] Ajout recaptcha sur form de contact

--- bolt/src/Asmb/Common/Controller/ContactController.php
+++ bolt/src/Asmb/Common/Controller/ContactController.php
@@ -43,20 +43,27 @@ class ContactController extends Base
 
         /** @var Form $form */
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var MailSender $mailSender */
-            $mailSender = $app['mail_sender'];
+            // validation du captcha
+            /** @see https://developers.google.com/recaptcha/docs/verify */
+            $captchaToken = $request->request->get('g-recaptcha-response');
+            if ($this->validCaptcha($request, $captchaToken)) {
+                /** @var MailSender $mailSender */
+                $mailSender = $app['mail_sender'];
 
-            $errorMessage = $mailSender->sendContactMail(
-                $form->get('contactEmail')->getData(),
-                $form->get('contactName')->getData(),
-                $form->get('contactSubject')->getData(),
-                $form->get('contactMessage')->getData()
-            );
+                $errorMessage = $mailSender->sendContactMail(
+                    $form->get('contactEmail')->getData(),
+                    $form->get('contactName')->getData(),
+                    $form->get('contactSubject')->getData(),
+                    $form->get('contactMessage')->getData()
+                );
 
-            if (!empty($errorMessage)) {
-                $this->flashes()->error('Votre message n\'as pas été envoyé.<br>Erreur : ' . $errorMessage);
+                if (!empty($errorMessage)) {
+                    $this->flashes()->error('Votre message n\'as pas été envoyé.<br>Erreur : ' . $errorMessage);
+                } else {
+                    $this->flashes()->success('Votre message a bien été envoyé, nous tâchons de vous répondre dans les meilleurs délais !<br><br>Le Bureau ASMB');
+                }
             } else {
-                $this->flashes()->success('Votre message a bien été envoyé, nous tâchons de vous répondre dans les meilleurs délais !<br><br>Le Bureau ASMB');
+                $this->flashes()->error('Votre message n\'as pas été envoyé.');
             }
         }
 
@@ -64,5 +71,51 @@ class ContactController extends Base
             'contenttypeslug' => 'page',
             'slug' => 'contact'
         ]);
+    }
+
+    private function validCaptcha(Request $request, ?string $token): bool
+    {
+        if (null === $token ) {
+            return false;
+        }
+
+        $fields = [
+            'secret' => $this->getOption('general/recaptcha/secret_key'),
+            'response' => $token,
+            'remoteip' => $request->getClientIp(),
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $this->getOption('general/recaptcha/api_site_verify_url'),
+            CURLOPT_CUSTOMREQUEST => $this->getOption('general/recaptcha/api_site_verify_method'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 20,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_USERAGENT => $request->headers->get('User-Agent'),
+            CURLOPT_POSTFIELDS => $fields,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+        ]);
+        $headers = [
+            'Content-Type' => 'application/json',
+        ];
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        $response = curl_exec($curl);
+        $infos = curl_getinfo($curl);
+        $error = curl_error($curl);
+
+        curl_close($curl);
+
+        if (Response::HTTP_OK === $infos['http_code']) {
+            $responseAsArray = json_decode($response, true);
+            return $responseAsArray['success'] ?? false;
+        }
+
+        return false;
     }
 }
